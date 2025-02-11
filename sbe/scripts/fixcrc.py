168c9c473af967c5090ed55fb708223afeee6ea4
Zane Shelley <zshelle@us.ibm.com>
2025-02-11 17:21:11 -0600

    Fix shebang in fixcrc.py

--- sbe/scripts/fixcrc.py
+++ sbe/scripts/fixcrc.py
@@ -1,25 +1,26 @@
-# 
+#!/usr/bin/env python3
+#
 # CRC-32 forcer (Python)
-# 
+#
 # Copyright (c) 2020 Project Nayuki
 # https://www.nayuki.io/page/forcing-a-files-crc-to-any-value
 #
 # Copyright (c) 2024 International Business Machines Corp.
 # https://github.com/open-power/op-utils/blob/main/sbe/scripts/fixcrc.py
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 import os, sys, zlib
 from typing import BinaryIO, List, Optional, Tuple
 # ---- Main application ----
